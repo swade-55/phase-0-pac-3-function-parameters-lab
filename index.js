a1286c25fcc3a49bb7e40ce041443d59bbbda7b3
SamuelWade <samuelrcwade@gmail.com>
2023-02-03 08:00:26 -0500

    Test Successful

---
+++ index.js
@@ -0,0 +1,16 @@
+function introduction(sam) {
+    return `Hi, my name is ${sam}.`;
+}
+
+function introductionWithLanguage(name,language) {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+}
+
+function introductionWithLanguageOptional(name,language) {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+}
+
+function introductionWithLanguageOptional(name,language='JavaScript') {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+}
+
